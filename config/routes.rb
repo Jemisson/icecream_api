9f758bfcd3f8e7259368c1d5793418ea0f5440d5
Jemisson <jemison321@gmail.com>
2025-01-23 18:43:21 -0400

    Demais itens da api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  #root "articles#index"
+  resources :users
+  resources :categories
+  resources :products
+  resources :sales
+  resources :rates
 end
